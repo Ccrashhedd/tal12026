3883ada346ad38d7fe0fe44299250024db27c819
Ccrashh <SMoralesF1403@gmail.com>
2026-03-31 15:36:39 -0500

    nuevo repo taller1

---
+++ DB/tal1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE TAL1;
+USE TAL1;  
+
+CREATE TABLE PRODUCTO (
+    idProducto BIGINT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    detalle TEXT NOT NULL,
+    precio DECIMAL(20,4) NOT NULL,
+    stock int NOT NULL
+);
